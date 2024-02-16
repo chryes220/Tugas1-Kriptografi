d883bba42045ecdff746b995fdf9a9a70a8a8786
Christine <christinehutabarat12@gmail.com>
2024-02-16 14:38:47 +0700

    feat: create submit controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,25 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def submit
+    act = params[:act]
+    cipher = params[:cipher]
+    message = params[:message]
+    key = params[:key]
+
+    case cipher
+    when "standard-vigenere"
+      cipher_class = StandardVigenere.instance
+      if act == "encrypt"
+        cipher_class.encrypt(message, key)
+      else
+        cipher_class.decrypt(message, key)
+      end
+    # ...
+    end
+
+  end
+
+  
 end
