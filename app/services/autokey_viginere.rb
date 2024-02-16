1f704c0fca73de2533a6c86a7cb305eb6957eeb4
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-02-16 19:52:22 +0700

    feat: add autokey viginere cipher (decrypt)

--- app/services/autokey_viginere.rb
+++ app/services/autokey_viginere.rb
@@ -36,5 +36,28 @@ class AutokeyViginere
   end
 
   def decrypt(ciphertext,key)
+    puts "Decrypting #{ciphertext} using key #{key}"
+
+    # Buang karakter non alfabet
+    ciphertext = ciphertext.gsub(/[^a-zA-Z]/,"")
+    key = key.gsub(/[^a-zA-Z]/,"")   
+
+    # Bila panjang key < ciphertext, tambahkan potongan ciphertext ke key sampai panjangnya sama
+    if key.length < ciphertext.length
+      key << ciphertext[0, ciphertext.length-key.length] 
+    end
+    
+    # lakukan dekripsi
+    plaintext = ""
+    ciphertext.each_char.with_index do |char,index|
+      char = char.downcase
+      key_char = key[index].downcase
+      # mestinya dah kesanitasi semua di awal
+      plaintext += (((char.ord - key_char.ord) % 26) + 97).chr
+    end
+    puts "Plaintext: #{plaintext}"
+    # Lu harus masukin kunci yang sama persis buat decrypt karena mana tau ini cipher kunci plaintextnya bjir
+    # return hasil
+    return {result: plaintext, result_base64: Base64.encode64(plaintext)}
   end
 end
