f623a673d1d642a764a2e817b8087487b1a46bbc
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-02-19 16:35:34 +0700

    feat: add hill cipher (decrypt)

--- app/services/hill.rb
+++ app/services/hill.rb
@@ -62,69 +62,53 @@ class Hill
   end
 
   def decrypt(ciphertext,key)
+    # parse key
+    temp_token = key.split(';')
+    # get m (dimension)
+    @m = temp_token[0].to_i
+    # parse key matrix
+    parse_key_matrix(temp_token[-1])
+    # inverskan matriks kunci
+    inverse_matrix()
+
     puts "Decrypting #{ciphertext} using key #{key}"
 
-  #   #ubah key ke lowercase
-  #   key = key.downcase
-  #   # Buat matriks kunci
-  #   for i in 0..4 do
-  #     for j in 0..4 do
-  #       @key_matrix[i][j] = key[5*i+j].ord - 97
-  #     end
-  #   end
+    # Buang karakter non alfabet
+    ciphertext = ciphertext.gsub(/[^a-zA-Z]/,"")
+    # konversi ke huruf kecil
+    ciphertext = ciphertext.downcase
+    # lakukan enkripsi
+    plaintext = ""
+    i = 0
+    ciphertext_array = Array.new(@m,0)
+    while i < ciphertext.length
+      # buat array berisi ciphertext dalam nilai ASCII relatif terhadap 'a'
+      if i+@m > ciphertext.length
+        offset = ciphertext.length-i
+        for j in 0...offset do
+          ciphertext_array[j] = (ciphertext[j+i].ord)-97
+        end
+        # kurang karakter, kasih padding
+        for j in offset...@m do
+          ciphertext_array[j] = 0
+        end
+      else
+        for j in 0...@m do
+          ciphertext_array[j] = (ciphertext[j+i].ord)-97
+        end
+      end
+      # konversi array ke matriks kolom
+      ciphertext_column_vector = Matrix.column_vector(ciphertext_array)
+      # kalikan dengan matriks kunci dengan rumus C = KP
+      result_matrix = @key_matrix * ciphertext_column_vector
+      # konversikan ke plaintext
+      plaintext << result_matrix.to_a.flatten.map { |element| ((element % 26) + 97).chr }.join
+      i += @m
+    end
 
-  #   # Buang karakter non alfabet
-  #   ciphertext = ciphertext.gsub(/[^a-zA-Z]/,"")
-  #   # Substitusi 'J' dengan 'i'
-  #   ciphertext = ciphertext.gsub(/[Jj]/,"i")
-
-  #   # Buat bigram
-  #   bigrams = [] # array of string
-  #   i = 0
-  #   while i < ciphertext.length
-  #     if i == ciphertext.length-1  
-  #       # tinggal sisa 1 huruf
-  #       bigrams << ciphertext[i].downcase + "x"
-  #       i += 1
-  #     elsif ciphertext[i]!=ciphertext[i+1] 
-  #       bigrams << ciphertext[i..i+1].downcase
-  #       i += 2
-  #     else
-  #       bigrams << ciphertext[i].downcase + "x"
-  #       i += 1
-  #     end
-  #   end
-
-  #   # lakukan enkripsi
-  #   plaintext = ""
-  #   # place bigrams
-  #   for bigram in bigrams
-  #     if is_same_row(bigram)
-  #       # untuk kasus baris yang sama
-  #       row = get_row(bigram[0])
-  #       bigram.each_char do |char|
-  #         plaintext << (@key_matrix[get_row_idx(row)][(get_col_idx(char,row)-1) % 5]+97).chr
-  #       end
-  #     elsif is_same_col(bigram)
-  #       # untuk kasus kolow yang sama
-  #       col = get_col_idx(bigram[0],get_row(bigram[0]))
-  #       bigram.each_char do |char|
-  #         plaintext << (@key_matrix[(get_row_idx(get_row(char))-1) %  5][col]+97).chr     
-  #       end  
-  #     else
-  #       # untuk kasus baris dan kolom berbeda
-  #       row_1 = get_row(bigram[0])
-  #       row_2 = get_row(bigram[1])
-  #       # karakter pertama, yang mana huruf pertama plaintext diganti dengan huruf pada perpotongan baris huruf pertama dengan kolom huruf kedua.
-  #       plaintext << (@key_matrix[get_row_idx(row_1)][get_col_idx(bigram[1],row_2)]+97).chr
-  #       # karakter kedua (sisanya), yang mana huruf kedua plaintext diganti dengan huruf pada perpotongan kolom huruf pertama dengan baris huruf kedua.
-  #       plaintext << (@key_matrix[get_row_idx(row_2)][get_col_idx(bigram[0],row_1)]+97).chr
-  #     end
-  #   end
-
-  #   puts "Plaintext: #{plaintext}"
-  #   # return hasil
-  #   return {result: plaintext, result_base64: Base64.encode64(plaintext)}
+    puts "Plaintext: #{plaintext}"
+    # return hasil
+    return {result: plaintext, result_base64: Base64.encode64(plaintext)}
   end
 
   def parse_key_matrix(key)
@@ -135,26 +119,23 @@ class Hill
     @key_matrix = Matrix[*converted_array]
   end
 
-  # def is_same_col(bigram)
-  #   cols = []
-  #   bigram.each_char do |char|
-  #     cols << get_col_idx(char,get_row(char))
-  #   end
-  #   # kalau sama semua, maka jika dibikin set akan memiliki panjang 1
-  #   return cols.uniq.size == 1
-  # end
-
-  # def get_row(element)
-  #   row = @key_matrix.detect{|line| line.include?(element.ord() -97)}
-  #   return row
-  # end
-
-  # def get_row_idx(row)
-  #   return @key_matrix.index(row)
-  # end
-
-  # def get_col_idx(element,row)
-  #   return row.index(element.ord-97) 
-  # end
-
+  def inverse_matrix()
+    # persamaan invers hill cipher: (KK^(-1)) mod 26 == I 
+    # persamaan determinannya: (det(K)* det(1/K)) mod 26 = 1
+    # (a*b) mod 26 = a mod 26 * b mod 26
+    # det(K) mod 26 * det(1/K) mod 26 = 1
+    # persamaan umum mencari invers: K^(-1) = adj(K)/det(K) = adj(K) * det(1/K)
+    det = @key_matrix.det % 26
+    # cari determinan matriks invers (invers dari det(K))
+    det_inv = 0
+    loop do
+      break if (det * det_inv) % 26 == 1
+      det_inv += 1
+    end
+    # cari adjoin matrix
+    adj_matrix = @key_matrix.adjugate
+    # K^(-1) = adj(K) * det(1/K)
+    # modulokan tiap elemen matriks dengan 26, kalikan dengan determinan K^(-1), lalu di modulokan lagi dengan 26
+    @key_matrix = adj_matrix.map{|e| ((e%26)*det_inv)%26}
+  end
 end
